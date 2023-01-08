d11a4c0b85c052117b51af6c0c3aab4804827175
lucassp12 <lucas_santos_pereira@hotmail.com>
2023-01-08 12:11:46 -0300

    Resolução

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,18 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists) {
+      throw new Error('User not exists!');
+    }
+
+    const userAdmin = this.usersRepository.turnAdmin(userExists);
+
+    return userAdmin;
   }
 }
 
